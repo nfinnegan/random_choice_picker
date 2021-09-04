7f11377d2626f07887d51be0f6d72dba1a4ec631
Natalie Finnegan <nfinneg2@gmail.com>
2021-09-03 21:12:36 -0500

    finished css, started JS

---
+++ script.js
@@ -0,0 +1,10 @@
+const tagsEl = document.getElementById("tags");
+const textarea = document.getElementById("textarea");
+
+textarea.focus();
+
+textarea.addEventListener("keyup", (e) => createTags(e.target.value));
+
+function createTags(input) {
+  console.log(input);
+}
