49a0297538985cf2c488b8b6f04577f4b832baaa
Natalie Finnegan <nfinneg2@gmail.com>
2021-09-03 21:22:25 -0500

    createTag fn to split choices, create el w/choices

--- script.js
+++ script.js
@@ -7,4 +7,17 @@ textarea.addEventListener("keyup", (e) => createTags(e.target.value));
 
 function createTags(input) {
   console.log(input);
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+
+  tagsEl.innerHTML = "";
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement("span");
+    tagEl.classList.add("tag");
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
 }
